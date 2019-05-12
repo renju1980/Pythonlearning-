ef67e7786589cf475516adc26325f13663c4fc83
renju1980 <50510510+renju1980@users.noreply.github.com>
2019-05-12 20:57:16 +0530

    Add files via upload

---
+++ StringConcatenate.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 12 20:53:30 2019
+
+Concatenate two strings v1 12052019
+"""
+
+First_str1 = 'Its Amazing'
+Second_str2 ='to learn Python today !'
+
+# using +
+print('First_str1 + Second_str2 = ', First_str1 + Second_str2)
