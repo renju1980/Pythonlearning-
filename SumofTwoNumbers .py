98482f05c12fac4face4c81c68b655c2b4961d0e
renju1980 <50510510+renju1980@users.noreply.github.com>
2019-05-12 20:52:17 +0530

    Add files via upload

---
+++ SumofTwoNumbers .py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is code for adding two numbers _ Ver1_12052019
+"""
+
+
+First_Num = int(input("enter first number: "))
+Second_Num= int(input("enter second number: "))
+ 
+sum = First_Num + Second_Num
+ 
+print("sum:", sum)
